0790ccd1421a983d7706bf89f28e9ea2c2c3bfc2
Mohab Ahmed Mohamed | Ejada Systems Ltd. <mohabahmed20005@gmail.com>
2024-03-25 11:22:15 +0200

    Blog Details Page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import Create from "./Create";
-import Home from "./Home";
 import Navbar from "./Navbar";
+import Home from "./Home";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Create from "./Create";
+import BlogDetails from "./BlogDetails";
+
 function App() {
   return (
     <Router>
       <div className="App">
         <Navbar />
-        <div className="content"></div>
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/create">
-            <Create />
-          </Route>
-        </Switch>
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/create">
+              <Create />
+            </Route>
+            <Route path="/blogs/:id">
+              <BlogDetails />
+            </Route>
+          </Switch>
+        </div>
       </div>
     </Router>
   );
