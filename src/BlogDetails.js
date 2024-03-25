e46b79d64f88ffd8a9d49a36151ff13fa14a4cc5
Mohab Ahmed Mohamed | Ejada Systems Ltd. <mohabahmed20005@gmail.com>
2024-03-25 12:25:21 +0200

    Project done

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,14 +1,22 @@
 import { useParams } from "react-router-dom";
 import useFetch from "./useFetch";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 const BlogDetails = () => {
+  const history = useHistory();
   const { id } = useParams();
   const {
     data: blog,
     error,
     isPending,
   } = useFetch("http://localhost:8000/blogs/" + id);
-
+  const handleClick = () => {
+    fetch("http://localhost:8000/blogs/" + blog.id, {
+      method: "DELETE",
+    }).then(() => {
+      history.push("/");
+    });
+  };
   return (
     <div className="blog-details">
       {isPending && <div>Loading...</div>}
@@ -18,6 +26,7 @@ const BlogDetails = () => {
           <h2>{blog.title}</h2>
           <p>Written by {blog.author}</p>
           <div>{blog.body}</div>
+          <button onClick={handleClick}>Delete Blog</button>
         </article>
       )}
     </div>
